81f775cf15f2b22eb34f53e35a95c2dccd39858f
jfsemideys <jfsemideys@hotmail.com>
2025-05-19 08:54:53 -0500

    Change to run in production and localhost

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 
+const Router = import.meta.env.DEV ? BrowserRouter : HashRouter;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-     <BrowserRouter basename="https://jfsemideys.github.io/jose-semidey-site/">
+     <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://jfsemideys.github.io/jose-semidey-site/',
+  base: process.env.NODE_ENV === 'production' ? 'https://jfsemideys.github.io/jose-semidey-site/' : '/',
   plugins: [react()],
 })
+ 
