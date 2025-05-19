59f0e112b9e8b6f72532a644bfafe4ad60abfb84
jfsemideys <jfsemideys@hotmail.com>
2025-05-19 09:02:52 -0500

    Change in vite.config to run local and production

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://jfsemideys.github.io/jose-semidey-site/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? 'https://jfsemideys.github.io/jose-semidey-site/' : '/',
   plugins: [react()],
-})
+}));
  
